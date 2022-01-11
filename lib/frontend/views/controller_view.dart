34642021d803e370b3e0caa7da5eda37687c2815
Cristian Stratulat <Cyber@Cristians-MacBook-Pro.local>
2022-01-11 20:05:29 +0200

    0.01

---
+++ lib/frontend/views/controller_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'home_view.dart';
+
+class controller_view extends StatefulWidget {
+  @override
+  _controller_viewState createState() => _controller_viewState();
+}
+
+class _controller_viewState extends State<controller_view> {
+  @override
+  Widget build(BuildContext context) {
+    return home_view();
+  }
+}
---
+++ lib/frontend/views/home_view.dart
@@ -0,0 +1,19 @@
+import 'package:autoworld/main.dart';
+import 'package:flutter/material.dart';
+
+class home_view extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('AutoWorld'),
+      ),
+      body: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
